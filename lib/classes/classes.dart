3abfca63e1052ac7d1f79a1050f5863a0da7deb8
Izaias Neto <izaiassaturninodelimaneto@hotmail.com>
2023-08-02 13:32:14 -0300

    Update classes.dart

--- lib/classes/classes.dart
+++ lib/classes/classes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Product {
-  int id;
   String name;
   String imgUrl;
   List<String> ingredientList;
@@ -9,14 +8,7 @@ class Product {
   String category;
   List<Product> acompanhamentosPossiveis;
 
-  Product(this.id, this.name, this.imgUrl, this.ingredientList, this.price, this.category, this.acompanhamentosPossiveis);
-
-  Widget displayViewItem(){
-    Widget w = Text(name);
-    // TODO
-    //display widget like in view page
-    return w;
-  }
+  Product(this.name, this.imgUrl, this.ingredientList, this.price, this.category, this.acompanhamentosPossiveis);
 }
 
 class Acompanhamento {
@@ -34,76 +26,83 @@ class Adicional {
   Adicional(this.name, this.price);
 }
 
-class ItemList { // carrinho, comparacao ou cardápio
-  // [
-  //  {
-  //      'product': Product,
-  //      'quantidade': 1,
-  //      'acompanhamentos': {'id': 5, 'quantidade': 1},
-  //      'adicionais': {'id': 6, 'quantidade': 1}
-  //  },
-  //  {
-  //      'product': Product,
-  //      'quantidade': 1,
-  //      'acompanhamentos': {'id': 3, 'quantidade': 1},
-  //      'adicionais': {'id': 4, 'quantidade': 1}
-  //  }
-  // ]
-  var items = [];
+//Map<int, Product> products; //cardapio
 
-  void addToList(Product p){
-    items.add(p);
-  }
+Product? getProductById(int id, Map<int, Product> products){
+  return products[id];
+}
 
-  void updateList(Product oldP, Product newP){
-    items.remove(oldP);
-    items.add(newP);
-  }
+class ItemList { // carrinho, comparacao
+  // var dictExample = {
+  //   1:
+  //     {
+  //       'quantidade': 2,
+  //       'acompanhamentos': [{ 5: {'quantidade': 1}, 6: {'quantidade': 1}, },{}],
+  //       'adicionais': [{ 7: {'quantidade': 1}, 8: {'quantidade': 1}, },{}],
+  //     },
+  //   2:
+  //     {
+  //       'quantidade': 3,
+  //       'acompanhamentos': [{},{},{}],
+  //       'adicionais': [{},{},{}],
+  //     },
+  // };
+  Map<dynamic, dynamic> items = {};
 
-  void removeFromList(Product p){
-    items.remove(p);
-  }
+  ItemList(this.items);
 
-  List<dynamic> filterByCategory(category){
-    var filteredItems = [];
-    for(var item in items){
-      if(item["product"].category == category){
-        filteredItems.add(item);
-      }
+  void addOneItemToList(int productId, ItemList acomp, ItemList adic){
+    if(items[productId] == null){
+      addToList(productId, 1, [acomp], [adic]);
     }
+    else{
+      items[productId]['quantidade'] += 1;
+      items[productId]['acompanhamentos'].add(acomp);
+      items[productId]['adicionais'].add(adic);
+    }
+  }
+
+  void addToList(int productId, int q, List<ItemList> acomp, List<ItemList> adic){
+    items[productId] = {
+      'quantidade': q,
+      'acompanhamentos': acomp,
+      'adicionais': adic
+    };
+  }
+
+  void removeOneItemFromList(int productId, ItemList acomp, ItemList adic){
+    items[productId]['quantidade'] -= 1;
+    items[productId]['acompanhamentos'].remove(acomp);
+    items[productId]['adicionais'].remove(adic);
+  }
+
+  void removeFromList(int productId){
+    items[productId]['quantidade'] = 0;
+  }
+
+  Map<dynamic, dynamic> filterByCategory(category){
+    var filteredItems = {};
+    items.forEach((k, v) => {
+      if(v['product']?.category == category){
+        filteredItems[k] = v
+      }
+    });
     return filteredItems;
   }
 
   List<dynamic> categories(){
     var categories = [];
-    for(var item in items){
-      var currentItemCategory = item["product"].category;
-      var currentItemImg = item["product"].imgUrl;
-      if(!categories.contains(currentItemCategory)){
-        categories.add({currentItemCategory, currentItemImg});
+    items.forEach((k, v) => {
+      if(!categories.contains(v["product"].category)){
+        categories.add({v["product"].category, v["product"].imgUrl})
       }
-    }
+    });
     return categories;
-  }
+  }  
 
-  List<Widget> displayNameAndImg(List<dynamic> nameAndImg){
-    List<Widget> nameAndImgWidgetList = [];
-    for(var nameAndImg in nameAndImg){
-      nameAndImgWidgetList.add(Text(nameAndImg[0]));
-      // TODO
-      //do something with url
-    }
-    return nameAndImgWidgetList;
-  }
-
-  List<Widget> displayComparison(){
-    List<Widget> comparisonWidgetList = [];
-    for(var item in items){
-      Product p = item["product"];
-      comparisonWidgetList.add(Text(p.name));
-      // TODO
-      //do something with other properties
-    }
-    return comparisonWidgetList;
+  List<Widget> displayItems(List<dynamic> items, Widget? button1, Widget? button2){
+    //TODO
+    //display widget list like in home page and items page
+    return [];
   }
 }
